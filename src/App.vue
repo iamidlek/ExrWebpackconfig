5b7d4ca29253f7fb2f7b163a32fb2558e2c92da9
iamidlek <iamidlek@naver.com>
2021-08-27 16:20:48 +0900

    TEMPLATE

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<h1>{{ message }}</h1>
+<TestImg />
+</template>
+
+<script>
+import TestImg from '~/components/LoadTest'
+export default {
+  components: {
+    TestImg
+  },
+  data() {
+    return {
+      message: 'Test'
+    }
+  }
+}
+</script>
