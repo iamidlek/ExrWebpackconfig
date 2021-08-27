c74db18baef9a87df1fa8b85e859aeaac79b3ebd
iamidlek <iamidlek@naver.com>
2021-08-27 16:57:01 +0900

    make handler

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <template>
-  <h1>
-    {{ message }}
+  <h1 @click="increase">
+    {{ count }}
   </h1>
   <TestImg />
 </template>
 
 <script>
-import TestImg from '~/components/LoadTest'
+// import TestImg from '~/components/LoadTest'
 export default {
-  components: {
-    TestImg
-  },
+  // components: {
+  //   TestImg
+  // },
   data() {
     return {
-      message: 'Test'
+      count: 0
     }
+  },
+  methods: {
+    increase(){
+      this.count += 1
+   }
   }
 }
 </script>
+
+<style lang=scss>
+  h1 {
+    font-size: 50px;
+    color: royalblue;
+  }
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
       {
         test: /\.s?css$/,
         use: [
-          'vue-style-loader',
+          // 'vue-style-loader',
           'style-loader', // 해석된 부분을 삽입해서 사용
           'css-loader',// 먼저 로드됨 js에서 css를 해석하는 용도
           'postcss-loader',
